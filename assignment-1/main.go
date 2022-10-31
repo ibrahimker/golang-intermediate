91603b2fc2620aa89918d3eb1783aa5c86d269e4
Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>
2022-10-31 11:20:36 +0700

    add put, delete, and get by id in assignment 1 (#6)
    Co-authored-by: Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>

--- assignment-1/main.go
+++ assignment-1/main.go
@@ -11,6 +11,7 @@ import (
 )
 
 type Todo struct {
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -27,8 +28,13 @@ const baseURL = "0.0.0.0:8080"
 // @BasePath /
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/todos", Create).Methods(http.MethodPost)
+
 	r.HandleFunc("/todos", Get).Methods(http.MethodGet)
+	r.HandleFunc("/todos/{id}", GetByID).Methods(http.MethodGet)
+	r.HandleFunc("/todos", Create).Methods(http.MethodPost)
+	r.HandleFunc("/todos/{id}", Put).Methods(http.MethodPut)
+	r.HandleFunc("/todos/{id}", Delete).Methods(http.MethodDelete)
+
 	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
 	// serve http server
@@ -52,6 +58,47 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Success add todo " + t.Name))
 }
 
+// Put is a handler for create todos API
+// @Summary Update new todos
+// @Description Update todos by ID
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} string
+// @Router /todos/{id} [put]
+func Put(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	for i := 0; i < len(Todos); i++ {
+		if Todos[i].ID == id {
+			var t Todo
+			decoder := json.NewDecoder(r.Body)
+			_ = decoder.Decode(&t)
+			Todos[i] = &t
+			w.Write([]byte("Success update todo " + t.ID))
+			return
+		}
+	}
+}
+
+// Delete is a handler for create todos API
+// @Summary Delete new todos
+// @Description Delete string by ID
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} string
+// @Router /todos/{id} [delete]
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	for i := 0; i < len(Todos); i++ {
+		if Todos[i].ID == id {
+			Todos = append(Todos[:i], Todos[i+1:]...)
+			w.Write([]byte("Success delete todo " + id))
+			return
+		}
+	}
+}
+
 // Get is a handler for get todos API
 // @Summary Get new todos
 // @Description get all todo list
@@ -64,4 +111,23 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	todosRes, _ := json.Marshal(Todos)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(todosRes)
+}
+
+// GetByID is a handler for get todos API
+// @Summary GetByID new todos
+// @Description get all todo list
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} string
+// @Router /todos/{id} [get]
+func GetByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	for i := 0; i < len(Todos); i++ {
+		if Todos[i].ID == id {
+			todosRes, _ := json.Marshal(Todos[i])
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(todosRes)
+		}
+	}
 }
