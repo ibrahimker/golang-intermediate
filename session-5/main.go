edbb3f031946ea227bc6096ad2afa4aa1550051c
Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>
2022-11-14 09:23:15 +0700

    introduction to echo

--- session-5/main.go
+++ session-5/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 )
@@ -33,6 +35,42 @@ func main() {
 	r.GET("/json", func(ctx echo.Context) error {
 		data := M{"Message": "Hello", "Counter": 2}
 		return ctx.JSON(http.StatusOK, data)
+	})
+
+	r.GET("/page1", func(ctx echo.Context) error {
+		name := ctx.QueryParam("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page2/:name", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page3/:name/*", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		message := ctx.Param("*")
+
+		data := fmt.Sprintf("Hello %s, I have message for you: %s", name, message)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.POST("/page4", func(ctx echo.Context) error {
+		name := ctx.FormValue("name")
+		message := ctx.FormValue("message")
+
+		data := fmt.Sprintf(
+			"Hello %s, I have message for you: %s",
+			name,
+			strings.Replace(message, "/", "", 1),
+		)
+
+		return ctx.String(http.StatusOK, data)
 	})
 
 	r.Start(":9000")
